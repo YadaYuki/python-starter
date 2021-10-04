0b9c0fee13286502af0cc62145b6456c1ca5b924
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-10-04 11:38:02 +0900

    Add Packages

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+from typing import List
+
 import requests
+from bs4 import BeautifulSoup
 
 
-def hoge(a: int) -> str:
-    return ""
+# htmlのテキストデータのみを抽出する。
+def parse_html(content: str) -> List[str]:
+    soup = BeautifulSoup(content, "lxml")
+    text_list: List[str] = []
+    div_item_list = soup.find_all("div")
+    for div_item in div_item_list:
+        text_list.append(div_item.get_text())
+    return text_list
 
 
-print(
-    requests.get(
-        "https://brightechshop.com/products/ambience-solar-1w?variant=8038410879012"
+if __name__ == "__main__":
+    html_doc = requests.get(
+        "https://brightech.com/products/ambience-solar-1w?variant=8038410879012"
     ).text
-)
+    # print(parse_html(html_doc))
+    print(parse_html("<div>hoge</div>"))
---
+++ test_main.py
@@ -0,0 +1,5 @@
+from main import parse_html
+
+
+def test_answer():
+    assert inc(3) == 5
